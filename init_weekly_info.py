0652e6ebfea19d276662973037eec5015b4e357a
tuanhk101 <nguyentuan.hk101@gmail.com>
2025-06-28 01:24:27 +0700

    Update weekly_report.py chuẩn

--- init_weekly_info.py
+++ init_weekly_info.py
@@ -1,22 +1,7 @@
-from db import connect  # hoặc từ weekly_db cũng được
+from db import connect
 
 def run_init():
-    th04_data = [
-        ("Khang", "TH04", 40),
-        ("Trung", "TH04", 50),
-        ("TH 001", "TH04", 50),
-        ("TH 002", "TH04", 50),
-        ("TH 003", "TH04", 50),
-        ("TH 004", "TH04", 50),
-        ("TH 005", "TH04", 50),
-        ("Aha Khang", "TH04", 55),
-        ("Dieu(Khang)", "TH04", 55),
-        ("Khang Ut 2", "TH04", 50),
-        ("Duy Khang", "TH04", 50),
-        ("Ut Khang", "TH04", 60),
-    ]
-
-    th01_data = [
+    data = [
         ("Co Huong", "TH01", 0),
         ("Goem", "TH01", 0),
         ("Huyền", "TH01", 0),
@@ -28,9 +13,6 @@ def run_init():
         ("Thuy", "TH01", 0),
         ("Bi", "TH01", 0),
         ("Atha", "TH01", 10),
-    ]
-
-    th02_data = [
         ("Family", "TH02", 35),
         ("Vinh", "TH02", 50),
         ("A6. A Tuan", "TH02", 30),
@@ -43,17 +25,31 @@ def run_init():
         ("A6.3", "TH02", 40),
         ("Phung D", "TH02", 40),
         ("Tuan(Vani)", "TH02", 30),
+        ("Khang", "TH04", 40),
+        ("Trung", "TH04", 50),
+        ("TH 001", "TH04", 50),
+        ("TH 002", "TH04", 50),
+        ("TH 003", "TH04", 50),
+        ("TH 004", "TH04", 50),
+        ("TH 005", "TH04", 50),
+        ("TH 006", "TH04", 50),
+        ("Aha Khang", "TH04", 55),
+        ("Dieu(Khang)", "TH04", 55),
+        ("Duy Khang", "TH04", 50),
+        ("Khang Ut 2", "TH04", 50),
+        ("Ut Khang", "TH04", 60)
     ]
-
-    all_data = th04_data + th01_data + th02_data
 
     with connect() as conn:
         cur = conn.cursor()
-        for name, group_master, rate in all_data:
+        for name, group_master, rate in data:
             cur.execute("""
                 INSERT INTO weekly_info (name, group_master, rate)
                 VALUES (%s, %s, %s)
                 ON CONFLICT DO NOTHING
             """, (name, group_master, rate))
         conn.commit()
-    print("✅ Done init DB")
+    print("✅ Weekly Info DB init done!")
+
+if __name__ == "__main__":
+    run_init()
